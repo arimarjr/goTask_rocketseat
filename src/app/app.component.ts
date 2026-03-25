2101f61b2189d3ceea525247acb1f3dc0c40188e
Arimar Silva Soares Junior <arimarjr@hotmail.com>
2026-03-25 11:39:11 -0300

    projeto GoTask pasta 3 - aula 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from "./components/components/header/header.component";
-import { MainContentComponent } from "./components/components/main-content/main-content.component";
-import { TaskFormModalComponent } from "./components/components/task-form-modal/task-form-modal.component";
+import { HeaderComponent } from './components/components/header/header.component';
+import { MainContentComponent } from './components/components/main-content/main-content.component';
+import { TaskFormModalComponent } from './components/components/task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from './components/components/task-comments-modal/task-comments-modal.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
     HeaderComponent,
     MainContentComponent,
-      TaskFormModalComponent
-],
+    TaskFormModalComponent,
+    TaskCommentsModalComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'projeto-go-task';
